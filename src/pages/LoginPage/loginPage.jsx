246b8e07c6be85119eecf5ddf3a9263f784377b7
RayenMiri <rayenmiri000@gmail.com>
2024-03-28 15:55:19 +0100

    fixed the margin

--- src/pages/LoginPage/loginPage.jsx
+++ src/pages/LoginPage/loginPage.jsx
@@ -12,11 +12,11 @@ const LoginPage = () => {
             <form className="space-y-4 md:space-y-6" action="#">
               <div>
                 <label htmlFor="email" className="flex justify-start mb-2 text-sm font-medium text-gray-900 ">Your email</label>
-                <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="name@company.com" required />
+                <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="example@email.com" required />
               </div>
               <div>
                 <label htmlFor="password" className="flex justify-start mb-2 text-sm font-medium text-gray-900">Password</label>
-                <input type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" required />
+                <input type="password" name="password" id="password" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" required />
               </div>
               <div className="flex items-center justify-between">
                 <div className="flex items-start">
--- src/pages/RegisterPage/registerPage.jsx
+++ src/pages/RegisterPage/registerPage.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const RegisterPage = () => {
   return (
     <div className="bg-white">
-      <div className="flex flex-col items-center justify-center pt-33 mx-auto lg:py-32">
+      <div className="flex flex-col items-center justify-center pt-20 mx-auto lg:py-18">
         <div className="w-full bg-white rounded-lg shadow md:mt-0 sm:max-w-md xl:p-0">
           <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
             <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl">
@@ -12,19 +12,19 @@ const RegisterPage = () => {
             <form className="space-y-4 md:space-y-6" action="#">
               <div>
                 <label htmlFor="firstname" className="flex justify-start mb-2 text-sm font-medium text-gray-900 ">First Name</label>
-                <input type="text" name="firstname" id="firstname" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" required />
+                <input type="text" name="firstname" id="firstname" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder='first name' required />
               </div>
               <div>
                 <label htmlFor="lastname" className="flex justify-start mb-2 text-sm font-medium text-gray-900 ">Last Name</label>
-                <input type="text" name="lastname" id="lastname" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" required />
+                <input type="text" name="lastname" id="lastname" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder='last name'required />
               </div>
               <div>
                 <label htmlFor="email" className="flex justify-start mb-2 text-sm font-medium text-gray-900 ">Your email</label>
-                <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="name@company.com" required />
+                <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="example@email.com" required />
               </div>
               <div>
                 <label htmlFor="password" className="flex justify-start mb-2 text-sm font-medium text-gray-900">Password</label>
-                <input type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" required />
+                <input type="password" name="password" id="password" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" required />
               </div>
               <button type="submit" className="w-full text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Register</button>
               <p className="text-sm font-light text-gray-500">
