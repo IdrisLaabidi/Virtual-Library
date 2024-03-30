56c942d67a4a8b279205429459fe7779fcc2a7be
RayenMiri <rayenmiri000@gmail.com>
2024-03-30 12:16:04 +0100

    addded dark mode feature

--- src/pages/LoginPage/loginPage.jsx
+++ src/pages/LoginPage/loginPage.jsx
@@ -1,66 +1,124 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-const LoginPage = (props) => {
-  const [email , setEmail] = useState('');
-  const [password , setPassword] = useState('');
+
+const LoginPage = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
   const navigate = useNavigate();
-  const handleSubmit = async (e) =>{
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    try{
-      const response = await fetch('http://localhost:4000/api/auth/login',{
-        method : 'POST',
-        body : JSON.stringify({email , password}),
-        headers : {
-          'Content-Type' : 'application/json'
+    try {
+      const response = await fetch('http://localhost:4000/api/auth/login', {
+        method: 'POST',
+        body: JSON.stringify({ email, password }),
+        headers: {
+          'Content-Type': 'application/json',
         },
-        credentials : 'include'
+        credentials: 'include',
       });
       const json = await response.json();
-      if(response.ok){
-        localStorage.setItem('user_id',json.user._id);
-        sessionStorage.setItem('user',JSON.stringify(json.user))
+      if (response.ok) {
+        localStorage.setItem('user_id', json.user._id);
+        sessionStorage.setItem('user', JSON.stringify(json.user));
         navigate('/home');
       }
-      if(!response.ok){
+      if (!response.ok) {
         console.log(json.message);
       }
-    }catch(error){
-      console.log(error)
+    } catch (error) {
+      console.log(error);
     }
-  }
+  };
 
   return (
-    <div className="bg-white">
+    <div className="bg-white dark:bg-gray-800 h-screen">
       <div className="flex flex-col items-center justify-center pt-33 mx-auto lg:py-32">
-        <div className="w-full bg-white rounded-lg shadow md:mt-0 sm:max-w-md xl:p-0">
+        <div className="w-full bg-white dark:bg-gray-700 rounded-lg shadow md:mt-0 sm:max-w-md xl:p-0">
           <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-            <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl">
+            <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 dark:text-gray-100 md:text-2xl">
               Sign in to your account
             </h1>
-            <form className="space-y-4 md:space-y-6" onSubmit={handleSubmit} method='POST'>
+            <form
+              className="space-y-4 md:space-y-6"
+              onSubmit={handleSubmit}
+              method="POST"
+            >
               <div>
-                <label htmlFor="email" className="flex justify-start mb-2 text-sm font-medium text-gray-900 ">Your email</label>
-                <input onChange={(e)=>{setEmail(e.target.value)}} type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="example@email.com" required />
+                <label
+                  htmlFor="email"
+                  className="flex justify-start mb-2 text-sm font-medium text-gray-900 dark:text-gray-300 "
+                >
+                  Your email
+                </label>
+                <input
+                  onChange={(e) => {
+                    setEmail(e.target.value);
+                  }}
+                  type="email"
+                  name="email"
+                  id="email"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 dark:bg-gray-600 dark:border-gray-500 dark:text-white sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" 
+                  placeholder="example@email.com"
+                  required
+                />
               </div>
               <div>
-                <label htmlFor="password" className="flex justify-start mb-2 text-sm font-medium text-gray-900">Password</label>
-                <input onChange={(e)=>{setPassword(e.target.value)}} type="password" name="password" id="password" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" required />
+                <label
+                  htmlFor="password"
+                  className="flex justify-start mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+                >
+                  Password
+                </label>
+                <input
+                  onChange={(e) => {
+                    setPassword(e.target.value);
+                  }}
+                  type="password"
+                  name="password"
+                  id="password"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 dark:bg-gray-600 dark:border-gray-500 dark:text-white sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" 
+                  required
+                />
               </div>
               <div className="flex items-center justify-between">
                 <div className="flex items-start">
                   <div className="flex items-center h-5">
-                    <input id="remember" aria-describedby="remember" type="checkbox" className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-blue-300"/>
+                    <input
+                      id="remember"
+                      aria-describedby="remember"
+                      type="checkbox"
+                      className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-blue-300"
+                    />
                   </div>
                   <div className="ml-3 text-sm">
-                    <label htmlFor="remember" className="text-gray-500">Remember me</label>
+                   <label
+                      htmlFor="remember"
+                      className= "text-gray-900 dark:text-gray-300"
+                    >
+                      Remember me
+                    </label>
                   </div>
                 </div>
-                <a href="#" className="text-sm font-medium text-blue-600 hover:underline">Forgot password?</a>
+                <a
+                  href="#"
+                  className="text-sm font-medium text-blue-600 hover:underline"
+                >
+                  Forgot password?
+                </a>
               </div>
-              <button type="submit" className="w-full text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Sign in</button>
-              <p className="text-sm font-light text-gray-500">
-                Don’t have an account yet? <a href="/register" className="font-medium text-blue-600 hover:underline">Sign up</a>
+              <button
+                type="submit"
+                className="w-full text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+              >
+                Sign in
+              </button>
+              <p className="text-sm font-light text-gray-900 dark:text-gray-300">
+                Don’t have an account yet?{' '}
+                <a href="/register" className="font-medium text-blue-600 hover:underline">
+                  Sign up
+                </a>
               </p>
             </form>
           </div>
--- src/pages/RegisterPage/registerPage.jsx
+++ src/pages/RegisterPage/registerPage.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { useState , useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 
-const RegisterPage = (props) => {
+const RegisterPage = () => {
   const [firstName , setFirstName] = useState('');
   const [lastName , setLastName] = useState('');
   const [email , setEmail] = useState('');
@@ -49,36 +49,93 @@ const RegisterPage = (props) => {
  
   return(
     <div className="h-screen">
-      {props.darkMode&&
-      <div className="bg-gray-800 h-full">
+      <div className="bg-white dark:bg-gray-800 h-full">
         <div className="flex flex-col items-center justify-center pt-16 mx-auto lg:py-18">
-          <div className="w-full bg-gray-700 rounded-lg shadow md:mt-0 sm:max-w-md xl:p-0">
+          <div className="w-full bg-white dark:bg-gray-700 rounded-lg shadow md:mt-0 sm:max-w-md xl:p-0">
             <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-              <h1 className="text-xl font-bold leading-tight tracking-tight text-white md:text-2xl">
+              <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 dark:text-white md:text-2xl">
                 Create your account
               </h1>
               <form className="space-y-4 md:space-y-6" onSubmit={handleSubmit} method='POST'>
                 <div>
-                  <label htmlFor="firstname" className="flex justify-start mb-2 text-sm font-medium text-gray-300 ">First Name</label>
-                  <input value={firstName} onChange={(e)=>{setFirstName(e.target.value)}} type="text" name="firstname" id="firstname" className="bg-gray-600 border border-gray-500 text-white sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder='first name' required />
+                  <label htmlFor="firstname" className="flex justify-start mb-2 text-sm font-medium text-gray-900 dark:text-gray-300 ">
+                    First Name
+                  </label>
+                  <input 
+                    value={firstName} 
+                    onChange={(e)=>{setFirstName(e.target.value)}} 
+                    type="text" 
+                    name="firstname" 
+                    id="firstname" 
+                    className="bg-gray-50 border border-gray-300 text-gray-900 dark:bg-gray-600 dark:border-gray-500 dark:text-white sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" 
+                    placeholder='first name' 
+                    required 
+                  />
                 </div>
                 <div>
-                  <label htmlFor="lastname" className="flex justify-start mb-2 text-sm font-medium text-gray-300 ">Last Name</label>
-                  <input value={lastName} onChange={(e)=>{setLastName(e.target.value)}} type="text" name="lastname" id="lastname" className="bg-gray-600 border border-gray-500 text-white sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder='last name'required />
+                  <label htmlFor="lastname" className="flex justify-start mb-2 text-sm font-medium text-gray-900 dark:text-gray-300 ">
+                    Last Name
+                  </label>
+                  <input 
+                    value={lastName} 
+                    onChange={(e)=>{setLastName(e.target.value)}} 
+                    type="text" 
+                    name="lastname" 
+                    id="lastname" 
+                    className="bg-gray-50 border border-gray-300 text-gray-900 dark:bg-gray-600 dark:border-gray-500 dark:text-white sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" 
+                    placeholder='last name'
+                    required 
+                  />
                 </div>
                 <div>
-                  <label htmlFor="email" className="flex justify-start mb-2 text-sm font-medium text-gray-300 ">Your email</label>
-                  <input value={email} onChange={(e)=>{setEmail(e.target.value)}} type="email" name="email" id="email" className="bg-gray-600 border border-gray-500 text-white sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="example@email.com" required />
+                  <label htmlFor="email" className="flex justify-start mb-2 text-sm font-medium text-gray-900 dark:text-gray-300 ">
+                    Your email
+                  </label>
+                  <input 
+                    value={email} 
+                    onChange={(e)=>{setEmail(e.target.value)}} 
+                    type="email" 
+                    name="email" 
+                    id="email" 
+                    className="bg-gray-50 border border-gray-300 text-gray-900 dark:bg-gray-600 dark:border-gray-500 dark:text-white sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" 
+                    placeholder="example@email.com" 
+                    required 
+                  />
                 </div>
                 <div>
-                  <label htmlFor="password" className="flex justify-start mb-2 text-sm font-medium text-gray-300">Password</label>
-                  <input value={password} onChange={(e)=>{setPassword(e.target.value)}} type="password" name="password" id="password" className="bg-gray-600 border border-gray-500 text-white sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" required />
+                  <label htmlFor="password" className="flex justify-start mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">
+                    Password
+                  </label>
+                  <input 
+                    value={password} 
+                    onChange={(e)=>{setPassword(e.target.value)}} 
+                    type="password" 
+                    name="password" 
+                    id="password" 
+                    className="bg-gray-50 border border-gray-300 text-gray-900 dark:bg-gray-600 dark:border-gray-500 dark:text-white sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" 
+                    required 
+                  />
                 </div>
                 <div>
-                  <label htmlFor="cpassword" className="flex justify-start mb-2 text-sm font-medium text-gray-300">Confirm Password</label>
-                  <input value={cPassword} onChange={(e)=>{setCPassword(e.target.value)}} type="password" name="cpassword" id="cpassword" className="bg-gray-600 border border-gray-500 text-white sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" required />
+                  <label htmlFor="cpassword" className="flex justify-start mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">
+                    Confirm Password
+                  </label>
+                  <input 
+                    value={cPassword} 
+                    onChange={(e)=>{setCPassword(e.target.value)}} 
+                    type="password" 
+                    name="cpassword" 
+                    id="cpassword" 
+                    className="bg-gray-50 border border-gray-300 text-gray-900 dark:bg-gray-600 dark:border-gray-500 dark:text-white sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" 
+                    required 
+                  />
                 </div>
-                <button type="submit" className="w-full text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Register</button>
+                <button 
+                  type="submit" 
+                  className="w-full text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+                >
+                  Register
+                </button>
                 <p className="text-sm font-light text-gray-400">
                   Already have an account? <a href="/login" className="font-medium text-blue-500 hover:underline">Sign in</a>
                 </p>
@@ -86,45 +143,9 @@ const RegisterPage = (props) => {
             </div>
           </div>
         </div>
-      </div>}
-      {!props.darkMode && <div className="bg-white h-full">
-      <div className="flex flex-col items-center justify-center pt-18 mx-auto lg:py-18">
-        <div className="w-full bg-white rounded-lg shadow md:mt-0 sm:max-w-md xl:p-0">
-          <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-            <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl">
-              Create your account
-            </h1>
-            <form className="space-y-4 md:space-y-6" onSubmit={handleSubmit} method='POST'>
-              <div>
-                <label htmlFor="firstname" className="flex justify-start mb-2 text-sm font-medium text-gray-900 ">First Name</label>
-                <input value={firstName} onChange={(e)=>{setFirstName(e.target.value)}} type="text" name="firstname" id="firstname" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder='first name' required />
-              </div>
-              <div>
-                <label htmlFor="lastname" className="flex justify-start mb-2 text-sm font-medium text-gray-900 ">Last Name</label>
-                <input value={lastName} onChange={(e)=>{setLastName(e.target.value)}} type="text" name="lastname" id="lastname" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder='last name'required />
-              </div>
-              <div>
-                <label htmlFor="email" className="flex justify-start mb-2 text-sm font-medium text-gray-900 ">Your email</label>
-                <input value={email} onChange={(e)=>{setEmail(e.target.value)}} type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="example@email.com" required />
-              </div>
-              <div>
-                <label htmlFor="password" className="flex justify-start mb-2 text-sm font-medium text-gray-900">Password</label>
-                <input value={password} onChange={(e)=>{setPassword(e.target.value)}} type="password" name="password" id="password" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" required />
-              </div>
-              <div>
-                <label htmlFor="cpassword" className="flex justify-start mb-2 text-sm font-medium text-gray-900">Confirm Password</label>
-                <input value={cPassword} onChange={(e)=>{setCPassword(e.target.value)}} type="password" name="cpassword" id="cpassword" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" required />
-              </div>
-              <button type="submit" className="w-full text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Register</button>
-              <p className="text-sm font-light text-gray-500">
-                Already have an account? <a href="/" className="font-medium text-blue-600 hover:underline">Sign in</a>
-              </p>
-            </form>
-          </div>
-        </div>
       </div>
-    </div>}
-    </div>  
+    </div>
+
   )
  
 };
