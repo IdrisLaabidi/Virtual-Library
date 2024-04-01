f9af2c31b09407272c33d9ee30b50fc632e192bb
IdrisLaabidi <idrislaabidi@gmail.com>
2024-04-01 20:29:28 +0100

    prep merge

--- src/pages/HomePage/Home.jsx
+++ src/pages/HomePage/Home.jsx
@@ -1,7 +1,19 @@
+import search from '../../assets/search-svgrepo-com.svg'
 
 const Home = () => {
     return ( <>
-    
+        <div className="w-full">
+            <div className="w-full bg-gray-400 h-24 rounded-md flex-col p-2" >
+                <h1>search in collection</h1>
+                <br className='w-full h-0'></br>
+                <div className="w-full bg-white border-none rounded-md flex justify-center items-center">
+                    <img className="h-8 w-8" alt="search icon" src={search}></img>
+                    <input className="w-full rounded-md border-none bg-transparent " placeholder='Search per colloction name '></input>
+                </div>
+                
+            </div>
+
+        </div>
     </> );
 }
  
