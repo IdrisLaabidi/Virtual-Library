5251faf30d9f6ae798946698297e0bff637bfe64
RayenMiri <rayenmiri000@gmail.com>
2024-04-08 14:14:57 +0100

    added animated illustration to the landing page

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,17 @@ import Login from './pages/LoginPage/loginPage';
 import Register from './pages/RegisterPage/registerPage';
 import Layout from './components/layout/layout';
 import AddItem from './pages/addItemPage/addItemPage'
-import { useEffect, useState } from 'react';
 import MyAccountPage from './pages/myAccount/myAccount';
 import Home from './pages/HomePage/Home';
 import MyLibraryPage from './pages/MyLibrary/myLibraryPage';
 import ViewCollection from './components/viewCollection/viewCollection';
+import LandingPage from './pages/landingPage/landingPage'
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route index element={<Home/>}/>
+          <Route index element={<LandingPage/>}/>
           <Route path="/login" element={<Login/>} />
           <Route path="/register" element={<Register/>} />
           <Route path="/home" element={<Layout children={<Home/>} path="/home" />}/>
