60efa60b2e5b12570976220098d507ccd66b25b8
Youssef Fathallah <fathallahyoussef09@gmail.com>
2024-04-13 19:01:18 +0100

    added the deleteItem and deleteCollection functionnalities to frontend

--- src/components/item/item.jsx
+++ src/components/item/item.jsx
@@ -1,6 +1,29 @@
-import itemCover from '../../assets/item-cover-placeholder.jpeg'
+import deleteIcon from '../../assets/delete-icon.svg'
+import Cookies from 'js-cookie'
+const token = Cookies.get("token")
 
 const ItemCard = ({item}) => {
+
+    const handleDelete = async () => {
+        try {
+            const response = await fetch(`http://localhost:4000/api/item/${item._id}`, {
+                method: 'DELETE',
+                headers: {
+                    'Authorization': 'Bearer '+token,
+                  },
+            });
+
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            alert('Item deleted successfuly!')
+
+            // Refresh the page or update the state here to reflect the deletion
+        } catch (error) {
+            console.error('Failed to delete item:', error);
+            alert('Failed to delete item!')
+        }
+    };
     return ( <div className="flex-col  justify-center items-center w-fit">
         <div key={item._id} className="bg-white dark:bg-gray-800 p-4 rounded-lg shadow-md">
                             <img src={item.itemImgURL} alt={`Cover of ${item.titre}`} className="w-auto h-auto object-cover mb-4 rounded-lg dark:text-white" />
@@ -10,8 +33,13 @@ const ItemCard = ({item}) => {
                             <p className="text-gray-600 dark:text-gray-200"><span className='dark:text-white font-semibold'>Type:</span> {item.type}</p>
                             <p className="text-gray-600 dark:text-gray-200"><span className='dark:text-white font-semibold'>Publication Date:</span> {new Date(item.publicationDate).toLocaleDateString()}</p>
                             <p className="text-gray-600 dark:text-gray-200"><span className='dark:text-white font-semibold'>Price:</span> {item.price}</p>
+                            <div>
+                                <img src={deleteIcon} 
+                                     alt='Delete Icon' 
+                                     className='h-10 w-10 cursor-pointer'
+                                     onClick={handleDelete}/>
+                            </div>
                         </div>
-        
     </div> );
 }
  
--- src/components/userBookshelf/userBookshelf.jsx
+++ src/components/userBookshelf/userBookshelf.jsx
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from 'react';
 import useFetch from '../../Hooks/useFetch';
 import {MoonLoader} from 'react-spinners'
 import {useNavigate} from 'react-router-dom'
+import Cookies from 'js-cookie'
+const token = Cookies.get("token")
 
 const UserBookshelf = () => {
     const userId = localStorage.getItem('user_id');
@@ -13,6 +15,27 @@ const UserBookshelf = () => {
             setUserCollection(userCollectionData);
         }
     }, [userCollectionData]);
+
+    const handleDelete = async ({collection}) => {
+        try {
+            const response = await fetch(`http://localhost:4000/api/collection/${collection._id}`, {
+                method: 'DELETE',
+                headers: {
+                    'Authorization': 'Bearer '+token,
+                  },
+            });
+
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            alert('Collection deleted successfuly!')
+
+            // Refresh the page or update the state here to reflect the deletion
+        } catch (error) {
+            console.error('Failed to delete collection:', error);
+            alert('Failed to delete collection!')
+        }
+    };
 
     return (
         <div className="bg-gray-100 dark:bg-gray-800 min-h-screen p-5 rounded-[10px]">
@@ -35,11 +58,18 @@ const UserBookshelf = () => {
                                 </div>
                                 <div className="p-5 border-t border-gray-200 dark:border-gray-600">
                                     <button 
-                                        className="text-indigo-300 hover:text-indigo-500 font-semibold"
+                                        className="text-indigo-300 hover:text-indigo-500 font-semibold mr-4"
                                         onClick={()=>{navigate(`/MyLibrary/viewCollection/${collection._id}`)}}
                                         >
                                             
                                         View Collection
+                                    </button>
+                                    <button 
+                                        className="text-red-700 hover:text-red-500 font-semibold"
+                                        onClick={()=>{handleDelete({collection})}}
+                                        >
+                                            
+                                        Delete Collection
                                     </button>
                                 </div>
                             </div>
